dbb0646c44af67af42b0467871f701d397a303e5
SAsadii <Sadra2ra@gmail.com>
2022-12-07 19:21:49 +0330

    commit message

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,20 @@
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          ca: env('DATABASE_CA')
+    // path: ./config/env/production/database.js
+
+    module.exports = ({ env }) => ({
+        connection: {
+          client: 'postgres',
+          connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: {
+              rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+            },
+          },
+          debug: false,
         },
-      },
-      debug: false,
-    },
-  });
+      });
+  
+  
