952d54dcfca8e7ebd39e46a853516f37e750201f
SAsadii <Sadra2ra@gmail.com>
2022-12-07 13:00:17 +0330

    Initial commit

---
+++ src/api/firstpage-item/routes/firstpage-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * firstpage-item router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::firstpage-item.firstpage-item');
